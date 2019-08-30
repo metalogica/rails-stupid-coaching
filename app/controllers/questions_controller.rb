c03a0290a6b622d6f636627940fcdd25de4b2dfc
Richard Jarram <rjarram@me.com>
2019-08-30 12:03:08 -0400

    added calendar

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,4 +8,7 @@ class QuestionsController < ApplicationController
     @response = "Great!" if @question == "I am going to work"
     @response = "Silly question, get dressed and go to work!" if @question[-1] == "?"
   end
+
+  def cal
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/ask', to: 'questions#ask', as: :ask
   get 'answer', to: 'questions#answer', as: :answer
+  get '/cal', to: 'questions#cal'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
